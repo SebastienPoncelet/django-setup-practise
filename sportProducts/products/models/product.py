0a39aedd513b6238914ddbc98a3ee4e4757c8809
Sebastien Poncelet <sebastienponcelet@hotmail.com>
2021-07-20 21:39:12 +0800

    [feature/setup-database]
    Update Product model to set foreign keys as not null. Require foreign keys.

--- sportProducts/products/models/product.py
+++ sportProducts/products/models/product.py
@@ -7,12 +7,12 @@ from .size import *
 from .availability import *
 
 class Product(models.Model):
-    colour = models.ForeignKey(Colour, null=True, on_delete=models.CASCADE, related_name='product')
-    sport = models.ForeignKey(Sport, null=True, on_delete=models.CASCADE, related_name='product')
-    category = models.ForeignKey(Category, null=True, on_delete=models.CASCADE, related_name='product')
-    brand = models.ForeignKey(Brand, null=True, on_delete=models.CASCADE, related_name='product')
-    size = models.ForeignKey(Size, null=True, on_delete=models.CASCADE, related_name='product')
-    availability = models.ForeignKey(Availability, null=True, on_delete=models.CASCADE, related_name='product')
+    colour = models.ForeignKey(Colour, on_delete=models.CASCADE, related_name='product')
+    sport = models.ForeignKey(Sport, on_delete=models.CASCADE, related_name='product')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='product')
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name='product')
+    size = models.ForeignKey(Size, on_delete=models.CASCADE, related_name='product')
+    availability = models.ForeignKey(Availability, on_delete=models.CASCADE, related_name='product')
     name = models.CharField(max_length=200)
     sku = models.CharField(max_length=200)
     variant = models.CharField(max_length=200)
